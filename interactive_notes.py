769665b20619d45fb816025901a85255c329b38b
CC Zhao <2417179763@sjtu.edu.cn>
2017-08-03 15:57:04 +0800

    Add files via upload

---
+++ interactive_notes.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn
+
+from PIL import Image
+from pylab import *
+
+image=array(Image.open('/home/charlie/Pictures/Wallpapers/vim.png'))
+imshow(image)
+print('please click 3 points')
+# the function ginput() in Pylab realizes interaction
+x=ginput(3)
+print('you clicked:',x)
+show()
