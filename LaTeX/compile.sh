2c92f857db088aed618ff60e35337aa76b957943
zcc31415926 <charliezhao1999@outlook.com>
2018-06-09 23:13:50 +0800

    add new LaTeX files

--- LaTeX/compile.sh
+++ LaTeX/compile.sh
@@ -1,6 +1,14 @@
-pdflatex test.tex
-bibtex test.aux
-pdflatex test.tex
-pdflatex test.tex
+str=$1
 
-rm test.aux test.log test.bbl test.blg test.toc
+pdflatex ${str%.*}.tex
+pdflatex ${str%.*}.tex
+
+if [ "$2" ];then
+    bibtex ${str%.*}.aux
+    pdflatex ${str%.*}.tex
+    pdflatex ${str%.*}.tex
+fi
+
+rm ${str%.*}.aux ${str%.*}.log ${str%.*}.toc
+rm ${str%.*}.bbl ${str%.*}.blg
+rm ${str%.*}.nav ${str%.*}.snm ${str%.*}.out
--- Shell/compile_latex.sh
+++ Shell/compile_latex.sh
@@ -1,6 +1,14 @@
-xelatex $1
-
 str=$1
 
-rm ${str%.*}.aux
-rm ${str%.*}.log
+pdflatex ${str%.*}.tex
+pdflatex ${str%.*}.tex
+
+if [ "$2" ];then
+    bibtex ${str%.*}.aux
+    pdflatex ${str%.*}.tex
+    pdflatex ${str%.*}.tex
+fi
+
+rm ${str%.*}.aux ${str%.*}.log ${str%.*}.toc
+rm ${str%.*}.bbl ${str%.*}.blg
+rm ${str%.*}.nav ${str%.*}.snm ${str%.*}.out
