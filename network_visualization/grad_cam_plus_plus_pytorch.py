c55b466c9dba8f01723d3c0b79ecd6885eabdab0
zcc31415926 <charliezhao1999@outlook.com>
2020-12-18 14:48:02 +0800

    201218-network_visualization

--- network_visualization/grad_cam_plus_plus_pytorch.py
+++ network_visualization/grad_cam_plus_plus_pytorch.py
@@ -1,4 +1,4 @@
-# class activation mapping
+# gradient class activation mapping ++
 
 import cv2
 import torch
@@ -8,15 +8,23 @@ def tmp(x):
     global gradients
     gradients = x
 
+def normalize(img, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+    for i in range(3):
+        img_slice = img[..., i]
+        img_slice -= np.mean(img_slice)
+        img_slice /= np.std(img_slice)
+        img[..., i] = img_slice * std[i] + mean[i]
+    return img
+
 def draw_cam(img_path, model, cam_path, cammed_img_path, img_size=(224, 224), cam_ratio=0.5):
     model.eval()
     img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img_input = cv2.resize(img, img_size, cv2.INTER_CUBIC) / 255.0
+    img_input = normalize(img_input)
     img_input = img_input.transpose(2, 0, 1)
     img_input = np.expand_dims(img_input, 0)
     img_input = torch.from_numpy(img_input).float()
-    # img_input.requires_grad_(True)
-    # img_input.register_hook(tmp)
 
     ########## model definition ##########
     # input: img_input
@@ -29,15 +37,20 @@ def draw_cam(img_path, model, cam_path, cammed_img_path, img_size=(224, 224), ca
     ########## model definition ##########
 
     pred_index = torch.argmax(output[0]).item()
+    print('prediction index', pred_index)
     pred_score = output[0, pred_index]
     features.register_hook(tmp)
     pred_score.backward()
     grad = gradients[0]
-    importance_factor = torch.nn.functional.adaptive_avg_pool2d(grad, (1, 1))
-    grad *= importance_factor
-    grad = torch.mean(grad, 0)
-    cam_map = grad.numpy()
-    cam_map = np.abs(cam_map)
+
+    features_sum = torch.sum(features[0], (1, 2), keepdim=True)
+    alpha = grad ** 2 / (2 * grad ** 2 + features_sum * grad ** 3 + 1e-10)
+    positive_grad = torch.nn.functional.relu(grad)
+    weight = torch.sum(alpha * positive_grad, (1, 2), keepdim=True)
+    cam_map = torch.sum(weight * features[0], 0)
+
+    cam_map = cam_map.detach().cpu().numpy()
+    cam_map = np.maximum(cam_map, 0)
     cam_map -= np.min(cam_map)
     cam_map /= np.max(cam_map)
 
@@ -53,6 +66,6 @@ if __name__ == '__main__':
     import torchvision
 
     model = torchvision.models.vgg16(pretrained=True)
-    img_path = './imagenetsub/n01669191/ILSVRC2012_val_00001662.JPEG'
+    img_path = '/home/charlie/Pictures/rosmt2.png'
     draw_cam(img_path, model, './cam_map.jpg', './cammed_img.jpg', (224, 224))
 
